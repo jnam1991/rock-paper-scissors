fec649902012c9cdf5a45b4f8a0c8f3b502adbd3
jnam1991 <nicholimiller7@gmail.com>
2024-03-13 13:01:44 -0700

    Add input and tie messege if both you and the cpu chose rock

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 let playerOneInput = prompt("Rock, paper, or scissors?", "Please type: rock, paper, or scissors");
+
+let CpuInput = "rock";
+
+if (playerOneInput === "rock" && CpuInput === "rock"){
+    console.log("You and your oponent both chose rock. Its a tie!");
+}
