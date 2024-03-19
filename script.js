9c65f3a04a574753a2722879eebc31555a330f15
jnam1991 <nicholimiller7@gmail.com>
2024-03-19 11:49:57 -0700

    Add all win, lose, and tie conditions

--- script.js
+++ script.js
@@ -3,5 +3,39 @@ let playerOneInput = prompt("Rock, paper, or scissors?", "Please type: rock, pap
 let CpuInput = "rock";
 
 if (playerOneInput === "rock" && CpuInput === "rock"){
+
     console.log("You and your oponent both chose rock. Its a tie!");
+
+} else if (playerOneInput === "rock" && CpuInput === "paper"){
+
+    console.log("You chose rock and your oponent chose paper. You lose.");
+
+} else if (playerOneInput === "rock" && CpuInput === "scissors"){
+
+    console.log("You chose rock and your oponent chose scissors. You win!");
+
+} else if (playerOneInput === "paper" && CpuInput === "paper"){
+
+    console.log("You and your oponent chose paper. Its a tie!");
+
+} else if (playerOneInput === "paper" && CpuInput === "scissors"){
+
+    console.log("You chose paper and your oponent chose scissors. You lose.");
+
+} else if (playerOneInput === "paper" && CpuInput === "rock"){
+
+    console.log("You chose paper and your oponent chose rock. You win!");
+
+} else if (playerOneInput === "scissors" && CpuInput === "scissors"){
+
+    console.log("You and your oponent chose scissors. Its a tie!");
+
+} else if (playerOneInput === "scissors" && CpuInput === "rock"){
+
+    console.log("You chose scissors and your oponent chose rock. You lose.");
+
+} else if (playerOneInput === "scissors" && CpuInput === "paper"){
+
+    console.log("You chose scissors and your oponent chose paper. You win!");
+
 }
